da7679e936de69a8068b8b4287b9058c834ed203
Cat <ligeia_undead@mail.ru>
2023-01-29 22:34:58 +0300

    программа ура это же ориентальчик!

---
+++ meow_001/Program.cs
@@ -0,0 +1,15 @@
+﻿//Это программа приветствия пользователя
+
+Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username == "Ориентальчик")
+{
+   Console.WriteLine("Ура, это же Ориентальчик!!!"); 
+}
+
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
